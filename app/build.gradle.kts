09c82b63ba3e1160ee11189c3f5659c16349fc1a
DevHyeok <leel135810@gmail.com>
2026-01-17 17:47:48 +0900

    기본적인 UI 단계1 전체적인 화면구성 Compose Navigation 구조

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,8 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    //추가
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.compose.ui:ui-text")
+    implementation("androidx.compose.ui:ui-text:1.6.3")
 }
