caeb53e9fea102edb2d9376c19384d3ec3f6dfda
2021158006 <2021158006@tukorea.ac.kr>
2026-02-23 19:17:38 +0900

    자세 분석 기능 추가(임시)

--- app/src/main/java/com/example/strapxml/VideoResources.kt
+++ app/src/main/java/com/example/strapxml/VideoResources.kt
@@ -22,7 +22,7 @@ class VideoResources : Fragment() {
     private val binding get() = _binding!!
 
     // 유튜브 API 키
-    private val YOUTUBE_API_KEY = "AIzaSyC2QpZp7vWCi8O8U3ghCmpqhE6mz9WyMXk"
+    private val YOUTUBE_API_KEY = "youtube_data_api"
 
     // 원본 데이터를 계속 가지고 있을 리스트
     private val fullList = mutableListOf<StretchingItem>()
